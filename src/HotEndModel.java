5c9f9442217e5040eb9cf32bf7a1fad5fbd2ed8d
reprappro <reprappro@gmail.com>
2012-08-28 13:21:49 +0100

    Optimiser to fit Hot End parameters added.

--- src/HotEndModel.java
+++ src/HotEndModel.java
@@ -12,7 +12,7 @@ import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
 /**
- * This is a simple program to model the behaviour of a RepRap hot end (or 
+ * This is a simple Java program to model the behaviour of a RepRap hot end (or 
  * anything else that gets heated) to allow the parameters of a PID for controlling
  * it to be set.
  * 
@@ -26,7 +26,7 @@ import org.jfree.data.xy.XYSeriesCollection;
  *   T0 is room temperature
  *   a and b are parameters that get set experimentally (see below).
  *   
- * For constant p, this ODE has the solution:
+ * For constant p, ODE (1) has the solution:
  *   
  *   T = T0 + a*p*[1 - e^(b(tLag - t))]/b  -----(2)
  *   
@@ -59,12 +59,10 @@ public class HotEndModel
 {
 
 	
-	double dt = 0.2;        // Time increment for Euler integration
-	double endTime = 120;    // How long to run the simulation (secs)
+	double dt = 0.4;        // Time increment for Euler integration
+	double endTime = 240;   // How long to run the simulation (secs)
 	double T0 = 25;         // Room temperature
-	double a = 0.15;        // Hot end parameter - see above
-	double b = 0.15;        // Hot end parameter - see above
-	double lag = 2;         // Time lag between power change and effect (secs)
+	double gradEst = 0.01;  // Used in numerical estimation of derivatives
 	
 	// PID variables
 	
@@ -72,16 +70,40 @@ public class HotEndModel
 	double eLast = 0;
 	double eIntegral = 0;
 	double pTest = 0.4;
-	double Kp = 0.005;
-	double Ki = 0.014;	
-	double Kd = 0.005;
+	double Kp = 0.001;
+	double Ki = 0.0014;	
+	double Kd = 0;
 	double clamp = 50;
 	double target = 205;
 	double range = 255;
 	
-	String dataFile = "/home/ensab/Pro/Git/Marlin/Slave/xx.pde";
+	String dataFile = "t04";
 	
 	double experiment[];
+	
+	class Parameters
+	{
+		// Initial guess values
+		
+		double a = 0.05;      // Hot end parameter - see above
+		double b = 0.02;      // Hot end parameter - see above
+		double lag = 5;       // Time lag between power change and effect (secs)
+		
+		Parameters() {}
+		
+		Parameters(double aa, double bb, double ll)
+		{
+			a = aa;
+			b = bb;
+			lag = ll;
+		}
+		
+		public String toString()
+		{
+			String r = "a: " + a + ", b: " + b + ", lag: " + lag;
+			return r;
+		}
+	}
 	
 	class Ring
 	{
@@ -90,9 +112,9 @@ public class HotEndModel
 		int in;
 		int out;
 		
-		Ring(double v)
+		Ring(double v, Parameters parm)
 		{
-			l = (int) (1 + lag/dt);
+			l = (int) (1 + parm.lag/dt);
 			buffer = new double[l];
 			in = 0;
 			out = l;
@@ -135,7 +157,7 @@ public class HotEndModel
 		{
 			e.printStackTrace();
 		}
-
+		T0 = experiment[0];
 	}
 	
     public void buildChart(XYSeries dataA, XYSeries dataB, XYSeries dataC) throws Exception 
@@ -167,28 +189,40 @@ public class HotEndModel
        chartFrame.setSize(800, 600);
     }  
 	
-	public double nextT(double T, double p, Ring r)
+    /**
+     * Take one Euler step in the ODE
+     * TODO: Change to Runge-Kutta?
+     * @param T
+     * @param p
+     * @param parm
+     * @param r
+     * @return
+     */
+	public double nextT(double T, double p, Parameters parm, Ring r)
 	{
-		double result = T + (a*r.get()*range - b*(T-T0))*dt;
+		double result = T + (parm.a*r.get()*range - parm.b*(T-T0))*dt;
 		r.add(p);
 		return result;
 	}
 	
-//	public XYSeries constantPower()
+//	public XYSeries constantPower(Parameters parm)
 //	{
 //		XYSeries result = new XYSeries( "Constant power: " + (int)(pTest*range) );
 //		double t = 0;
 //		double T = T0;
-//		Ring r = new Ring(0);
+//		Ring r = new Ring(0, parm);
 //		while(t < endTime)
 //		{
 //			result.add(t, T); 
-//			T = nextT(T, pTest, r);
+//			T = nextT(T, pTest, parm, r);
 //			t += dt;
 //		}
 //		return result;
 //	}
 	
+	/**
+	 * Return the power demanded by the PID
+	 */
 	public double PID(double T)
 	{
 		eLast = e;
@@ -200,40 +234,181 @@ public class HotEndModel
 		return result;
 	}
 	
-	public XYSeries PIDPower()
+	/**
+	 * Plot curve for the PID response
+	 * @param parm
+	 * @return
+	 */
+	public XYSeries PIDPower(Parameters parm)
 	{
 		XYSeries result = new XYSeries( "PID power");
 		double t = 0;
 		double T = T0;
-		Ring r = new Ring(0);
+		Ring r = new Ring(0, parm);
 		while(t < endTime)
 		{
 			result.add(t, T);
 			double p = PID(T);
-			T = nextT(T, p, r);
+			T = nextT(T, p, parm, r);
 			t += dt;
 		}
 		return result;
 	}
 	
-	public double odeSolution(double t)
+	/**
+	 * Return the analytical solution at time t for constant power pTest
+	 * @param t
+	 * @param parm
+	 * @return
+	 */
+	public double odeSolution(double t, Parameters parm)
 	{
-		if(t <= lag) return T0;
-		return T0 + a*pTest*range*( 1 - Math.exp(b*(lag - t)) )/b;
+		if(t <= parm.lag) return T0;
+		return T0 + parm.a*pTest*range*( 1 - Math.exp(parm.b*(parm.lag - t)) )/parm.b;
 	}
 	
-	public XYSeries analyticalSolution()
+	/**
+	 * Plot curve for the analytical solution at constant power pTest
+	 * @param parm
+	 * @return
+	 */
+	public XYSeries analyticalSolution(Parameters parm)
 	{
 		XYSeries result = new XYSeries( "Theory");
 		double t = 0;
 		while(t < endTime)
 		{
-			result.add(t, odeSolution(t));
+			result.add(t, odeSolution(t, parm));
 			t += dt;
 		}
 		return result;
 	}
 	
+	/**
+	 * Compute the residual sum of squares between the analytical solution for
+	 * parm and the experiment.
+	 * @param parm
+	 * @return
+	 */
+	public double rss(Parameters parm)
+	{
+		double r = 0;
+		double t = 0;
+		int i = 0;
+		while(t < endTime)
+		{
+			double d = odeSolution(t, parm) - experiment[i];
+			r += d*d;
+			t += dt;
+			i++;
+		}
+		return r;
+	}
+	
+	/**
+	 * Compute the partial derivative of the RSS w.r.t. parm.a
+	 * @param r
+	 * @param parm
+	 * @return
+	 */
+	public double drByDa(double r, Parameters parm)
+	{
+		double delta = Math.abs(parm.a*gradEst);
+		Parameters temp = new Parameters(parm.a + delta, parm.b, parm.lag);
+		double newR = rss(temp);
+		return (newR - r)/delta;
+	}
+	
+	/**
+	 * Compute the partial derivative of the RSS w.r.t. parm.b
+	 * @param r
+	 * @param parm
+	 * @return
+	 */
+	public double drByDb(double r, Parameters parm)
+	{
+		double delta = Math.abs(parm.b*gradEst);
+		Parameters temp = new Parameters(parm.a, parm.b + delta, parm.lag);
+		double newR = rss(temp);
+		return (newR - r)/delta;
+	}
+	
+	/**
+	 * Compute the partial derivative of the RSS w.r.t. parm.lag
+	 * @param r
+	 * @param parm
+	 * @return
+	 */
+	public double drByDlag(double r, Parameters parm)
+	{
+		double delta = Math.abs(parm.lag*gradEst);
+		Parameters temp = new Parameters(parm.a, parm.b, parm.lag + delta);
+		double newR = rss(temp);
+		return (newR - r)/delta;
+	}
+	
+	/**
+	 * Dumb descend-the-slope optimiser to fit the parameters to the
+	 * experiment.
+	 * @return
+	 */
+	public Parameters fit()
+	{
+		Parameters pr = new Parameters();
+		double r = rss(pr);
+		double newR;
+		Parameters newPr;
+		int count = 0;
+		double g;
+		while(count < 200 && Math.sqrt(r)*dt/endTime > 0.1)
+		{
+			g = drByDa(r, pr);
+			if(g > 0)
+				newPr = new Parameters(pr.a - Math.abs(pr.a*gradEst), pr.b, pr.lag);
+			else
+				newPr = new Parameters(pr.a + Math.abs(pr.a*gradEst), pr.b, pr.lag);
+			newR = rss(newPr);
+			if(newR < r)
+			{
+				r = newR;
+				pr = newPr;
+			}
+			
+			g = drByDb(r, pr);
+			if(g > 0)
+				newPr = new Parameters(pr.a, pr.b - Math.abs(pr.b*gradEst), pr.lag);
+			else
+				newPr = new Parameters(pr.a, pr.b + Math.abs(pr.b*gradEst), pr.lag);
+			newR = rss(newPr);
+			if(newR < r)
+			{
+				r = newR;
+				pr = newPr;
+			}
+			
+			g = drByDlag(r, pr);
+			if(g > 0)
+				newPr = new Parameters(pr.a, pr.b, pr.lag - Math.abs(pr.lag*gradEst));
+			else
+				newPr = new Parameters(pr.a, pr.b, pr.lag + Math.abs(pr.lag*gradEst));
+			newR = rss(newPr);
+			if(newR < r)
+			{
+				r = newR;
+				pr = newPr;
+			}
+			
+			count++;
+		}
+		System.out.println("RMS error: " + Math.sqrt(r)*dt/endTime + ", iterations: " + count);
+		System.out.println("Parameters: " + pr);
+		return pr;
+	}
+	
+	/**
+	 * Plot curve for the experimental results
+	 * @return
+	 */
 	public XYSeries experimentalObservations()
 	{
 		XYSeries result = new XYSeries( "Experiment");
@@ -250,14 +425,16 @@ public class HotEndModel
 	
 	public static void main(String[] args) 
 	{
-	  HotEndModel hem = new HotEndModel();
-      XYSeries pid = hem.PIDPower();
-      XYSeries analytical = hem.analyticalSolution();
-      XYSeries expt = hem.experimentalObservations();
-		try {
+		HotEndModel hem = new HotEndModel();
+		Parameters pr = hem.fit();
+		XYSeries pid = hem.PIDPower(pr);
+		XYSeries analytical = hem.analyticalSolution(pr);
+		XYSeries expt = hem.experimentalObservations();
+		try 
+		{
 			hem.buildChart(expt, analytical, pid);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
+		} catch (Exception e) 
+		{
 			e.printStackTrace();
 		}  
 	}
